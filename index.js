7e7d022fbf9039383a3158540a50c4451f3bdbb2
Saurav Uppoor <sauravsuppoor@gmail.com>
2021-07-24 11:40:38 +0530

    Change port

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 // for demonstration purpose
 simulate();
